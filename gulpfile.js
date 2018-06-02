2b7620d7e09fcc43a8b894363bf3f8cd11bf6f64
Simon Asika <asika32764@gmail.com>
2018-06-02 18:35:58 +0800

    Code style

--- gulpfile.js
+++ gulpfile.js
@@ -6,31 +6,31 @@
  */
 
 var gulp = require("gulp"),//http://gulpjs.com/
-	util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
-	sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
-	autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
-	minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
-	rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
-	log = util.log;
+  util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
+  sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
+  autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
+  minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
+  rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
+  log = util.log;
 
 var sassFiles = "src/**/*.scss";
 var entryFile = "src/main.scss";
 
-gulp.task("sass", function(){
-	log("Generate CSS files " + (new Date()).toString());
-	gulp.src(entryFile)
-		.pipe(rename('silicone'))
-		.pipe(sass({ style: 'expanded' }))
-		.pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
-		.pipe(gulp.dest("dist"))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('dist'));
+gulp.task("sass", function () {
+  log("Generate CSS files " + (new Date()).toString());
+  gulp.src(entryFile)
+    .pipe(rename('silicone'))
+    .pipe(sass({style: 'expanded'}))
+    .pipe(autoprefixer("last 3 version", "safari 5", "ie 8", "ie 9"))
+    .pipe(gulp.dest("dist"))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task("watch", function(){
-	log("Watching scss files for modifications");
-	gulp.watch(sassFiles, ["sass"]);
+gulp.task("watch", function () {
+  log("Watching scss files for modifications");
+  gulp.watch(sassFiles, ["sass"]);
 });
 
 gulp.task("default", ["sass", "watch"]);
